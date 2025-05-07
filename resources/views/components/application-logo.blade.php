b0c314fbbe208d88cb62249ac906a49a2835477c
Wilberth Loría <stwilberth@gmail.com>
2025-05-06 20:56:44 -0600

    limpio

--- resources/views/components/application-logo.blade.php
+++
@@ -1,5 +0,0 @@
-<img
-    src="{{ asset('logo_transparent.png') }}"
-    alt="Anúncielo.com Logo"
-    class="h-full w-auto object-contain max-h-12"
-/>
--- resources/views/components/primary-button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-amethyst-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-amethyst-600 focus:bg-amethyst-600 active:bg-amethyst-900 focus:outline-none focus:ring-2 focus:ring-amethyst-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</button>
